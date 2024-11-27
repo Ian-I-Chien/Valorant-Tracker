b2d1e7b5640d9f034b653b22ba569aafc9be29b1
XMAX <xmax861231@gmail.com>
2024-11-27 21:28:47 +0800

    add global for toxic_detector

--- nlp.py
+++ nlp.py
@@ -4,6 +4,7 @@ from model.toxic_detector import ToxicDetector
 toxic_detector: ToxicDetector = None
 
 async def nlp_process(message: discord.Message):
+    global toxic_detector
     if toxic_detector is None:
         toxic_detector = ToxicDetector()
     result = toxic_detector.detect(message.content)
