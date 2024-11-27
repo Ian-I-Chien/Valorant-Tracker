18d0c74297ac5e85ce699abae3fbdd337b78d6f4
XMAX <xmax861231@gmail.com>
2024-11-27 21:07:32 +0800

    change model to "IDEA-CCNL/Erlangshen-Roberta-110M-Sentiment"

--- util/ToxicDetector.py
+++ util/ToxicDetector.py
@@ -3,14 +3,14 @@ from deep_translator import GoogleTranslator
 
 class ToxicDetector:
     def __init__(self) -> None:
-        self.model_name = "unitary/toxic-bert"
+        self.model_name = "IDEA-CCNL/Erlangshen-Roberta-110M-Sentiment"
         self.classifier = pipeline(task="text-classification", model=self.model_name, tokenizer=self.model_name)
     
     def google_translate(self, text: str) -> str:
         return GoogleTranslator(source='auto', target='en').translate(text)
     
     def detect(self, text: str):
-        text = self.google_translate(text=text)
+        # text = self.google_translate(text=text)
         result: dict = self.classifier(text)[0]
         if result.get("score", 0) > 0.5:
             return True
