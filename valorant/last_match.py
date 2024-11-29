01fa2fba53770ca8fbfa35eea1ef153cc54ef299
Ian-I-Chien <chieni1121@gmail.com>
2024-11-30 03:13:03 +0800

    Add detail rank info while Last Match is Competitive

--- valorant/last_match.py
+++ valorant/last_match.py
@@ -1,10 +1,12 @@
 import os
 import json
 import math
+import time
 import asyncio
 import discord
-from .api import fetch_json, url_json
 from .player import ValorantPlayer
+from .api import fetch_json, url_json
+
 
 class LastMatch:
     def __init__(self, player_name, player_tag, region="ap"):
@@ -14,46 +16,68 @@ class LastMatch:
         self.player_tag = player_tag
         self.region = region
 
-    async def sorted_player(self):
+    async def get_rank_with_retries(self, player_instance, retries=5, delay=2):
+        attempt = 0
+        while attempt < retries:
+            rank_data_dict = await player_instance.get_rank()
+            if rank_data_dict:
+                return rank_data_dict
+            attempt += 1
+            await asyncio.sleep(delay)
+        return None
+
+    async def sorted_formatted_player(self):
         sorted_players = sorted( self.last_match_data['data']['players']['all_players'], key=lambda x: x['stats']['score'], reverse=True)
         formatted_info = ""
 
         for index, player in enumerate(sorted_players):
 
-            if self.last_match_data['data']['metadata']['mode'] == "Competitive":
-                rank_data = player['currenttier_patched']
-            else:
-                player_instance = ValorantPlayer(player_name=player['name'], player_tag=player['tag'])
-                rank_data_dict = await player_instance.get_rank()
-                rank_data = rank_data_dict.get('currenttierpatched', 'Unrated') if rank_data_dict else 'Unrated'
+            player_instance = ValorantPlayer(player_name=player['name'], player_tag=player['tag'])
+            rank_data_dict = await self.get_rank_with_retries(player_instance)
 
+            current_tier = rank_data_dict.get('currenttierpatched', 'Unrated') if rank_data_dict else 'Unrated'
+
+            if self.last_match_data['data']['metadata']['mode'] == "Competitive":
+                rank_in_tier = rank_data_dict['ranking_in_tier']
+                mmr_change   = rank_data_dict['mmr_change_to_last_game']
+            else:
+                rank_in_tier = None
+                mmr_change   = None
 
             score = math.floor(player['stats']['score'] / self.last_match_data['data']['metadata']['rounds_played'])
             total_shots = player['stats']['bodyshots'] + player['stats']['headshots'] + player['stats']['legshots']
             headshot_percentage = (player['stats']['headshots'] / total_shots) * 100 if total_shots > 0 else 0
 
             formatted_info += "`{}`\n".format(
-                f"[{player['team'][0]}] [{rank_data}] "
+                f"[{player['team'][0]}] [{current_tier}] "
                 f"[{player['name']}#{player['tag']}] "
             )
 
             formatted_info += "`{}`\n".format(
                 f"{player['character']} "
                 f"{player['stats']['kills']}/{player['stats']['deaths']}/{player['stats']['assists']} "
-                f"[{headshot_percentage:.2f}%]"
+                f"[{headshot_percentage:.2f}%] "
                 f"[{score}]"
             )
+
+            if rank_in_tier and mmr_change:
+                formatted_info += "`{}`\n".format(
+                    f"[{rank_in_tier}/99] "
+                    f" [{mmr_change:+d}]"
+                )
+
+        blue_wins = self.last_match_data['data']['teams']['blue']['rounds_won']
+        red_wins = self.last_match_data['data']['teams']['red']['rounds_won']
+        winning_team = "BLUE" if blue_wins > red_wins else "RED" if blue_wins < red_wins else "TIED"
+
         ratio = (
-            f"{self.last_match_data['data']['teams']['blue']['rounds_won']}:{self.last_match_data['data']['teams']['blue']['rounds_lost']}"
-            if self.last_match_data['data']['rounds'][0]['winning_team'].lower() == "blue"
-            else
-            f"{self.last_match_data['data']['teams']['red']['rounds_won']}:{self.last_match_data['data']['teams']['red']['rounds_lost']}"
+            f"{blue_wins}:{red_wins}"
         )
 
         title_info = "{}".format(
             f"Last Match\n"
             f"{self.last_match_data['data']['metadata']['mode']}\t"
-            f"{self.last_match_data['data']['rounds'][0]['winning_team']} WIN!\t"
+            f"{winning_team} WIN!\t"
             f"[{ratio}]"
         )
 
@@ -68,7 +92,7 @@ class LastMatch:
         self.last_match_data = await fetch_json(url)
         if not self.last_match_data:
             return None
-        return await self.sorted_player()
+        return await self.sorted_formatted_player()
 
 
     async def get_last_match_id(self):
