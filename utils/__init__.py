a0ab42af54ad81b312476e1f4ead871baa8e6ce0
Ian Chien <err@csie.io>
2026-08-29 12:12:09 +0800

    refactor: remove legacy channel export

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,3 @@
-from .utils import fix_isoformat, get_env_or_interaction_channel, parse_player_name
+from .utils import fix_isoformat, parse_player_name
 
-__all__ = ["fix_isoformat", "get_env_or_interaction_channel", "parse_player_name"]
+__all__ = ["fix_isoformat", "parse_player_name"]
